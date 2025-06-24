51eedd4338a13357a7cc864db1bf2f1350f8c2ee
maskadev <sidkiyouness@gmail.com>
2025-06-24 15:55:42 +0100

    feature : setup backend api implimentation

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
+import './index.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { PrimeReactProvider } from 'primereact/api';
+import App from "./App.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query.ts";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.tsx";
+
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <PrimeReactProvider>
-        <App />
-      </PrimeReactProvider>
-  </StrictMode>,
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <AuthProvider>
+                    <PrimeReactProvider>
+                        <App />
+                    </PrimeReactProvider>
+                </AuthProvider>
+            </BrowserRouter>
+        </QueryClientProvider>
+    </StrictMode>
 )
