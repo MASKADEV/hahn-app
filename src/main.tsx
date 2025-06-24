0de8108d75013e144dd840a7751bec084baa48d2
maskadev <sidkiyouness@gmail.com>
2025-06-24 18:01:43 +0100

    improve: proper naming

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { PrimeReactProvider } from 'primereact/api';
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/react-query.ts";
+import { queryClient } from "./lib/reactQuery.ts";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
